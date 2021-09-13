1d09d9b6a8ec5f6e4d09646df6e7341ac89b9137
Rilazy <35211613+Rilazy@users.noreply.github.com>
2021-09-13 14:57:51 -0400

    Added a little bob

--- io-racer/Assets/cubeMove.cs
+++ io-racer/Assets/cubeMove.cs
@@ -7,12 +7,19 @@ public class cubeMove : MonoBehaviour
     // Start is called before the first frame update
     float accel = 0f;
     public float hoverHeight;
+    public float hoverStrength; //A multiplier to the output of the smooth wave generator to determine the max and min height of the bobbing
     public float maxAccel;
     public GameObject road;
+    byte index; //Keeps track of index in lookup table
+    float[] sineLookup = new float[256]; //Lookup table for sine values (so we don't need to compute them at runtime)
 
     void Start()
     {
-
+        
+        for (float i = 0; i <= 255; i++)
+        {
+            sineLookup[(int)i] = (Mathf.Sin(Mathf.Deg2Rad * (i * (360f / 255f))) * hoverStrength);
+        }
     }
 
     // Update is called once per frame
@@ -31,17 +38,19 @@ public class cubeMove : MonoBehaviour
     void roadFollow(){
         RaycastHit hit;
         float roadY;
+        
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
         {
             roadY = hit.point.y;
             Vector3 Position = this.GetComponent<Transform>().position;
-            this.GetComponent<Transform>().position = new Vector3(Position.x, roadY + hoverHeight, Position.z);
+            this.GetComponent<Transform>().position = new Vector3(Position.x, roadY + hoverHeight + sineLookup[index], Position.z);
         }
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 1000, Color.white);
             Debug.Log("Did not Hit");
         }
+        index++;
     }
 }
